aeb81106398bd91ba7bc937e0e041a4d836c5d9a
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-09-25 21:26:27 -0300

    Create introducao.py

---
+++ aula_1/introducao.py
@@ -0,0 +1,11 @@
+#print com soma de int e string
+valor1 = 30
+valor2 = 70
+print(valor1 + valor2)
+valor1 = "30"
+valor2 = "70"
+print(valor1 + valor2)
+#INPUT (tudo digitado em input sai em formato de string)
+print("Digite seu nome:")
+nome = input("")
+print("O seu nome é: " + nome)
