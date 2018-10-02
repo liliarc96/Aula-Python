323d0a0e0de0424004cfdf1abe596d603d743ac9
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-02 20:49:58 -0300

    Update and rename lol.py to soma.py

---
+++ aula-2 /soma.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia dois números inteiros calcula e exibe a soma entre eles
+
+n1 = int(input("Digite o primeiro número:"))
+n2 = int(input("Digite o segundo número:"))
+
+soma = n1+n2
+
+print("A soma dos dois números é igual à ",soma,".")
+
