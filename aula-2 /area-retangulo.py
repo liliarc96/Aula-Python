b3700568f78d792ad3b8596116e93a5099fa1d59
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-02 20:55:47 -0300

    Create area-retangulo.py

---
+++ aula-2 /area-retangulo.py
@@ -0,0 +1,8 @@
+#Faça um algoritmo que lê a base e a altura de um retângulo, calcula e exibe sua área;
+
+base = int(input("Digite a base do retângulo:"))
+altura = int(input("Digite a altura do retângulo:"))
+
+area = base * altura
+
+print("A área do retângulo é de",area,"metros quadrados.")
