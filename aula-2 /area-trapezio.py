c0e94a0e4e950ad4039ee7e9d7e0d38d6699802f
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-02 21:04:48 -0300

    Create area-trapezio.py

---
+++ aula-2 /area-trapezio.py
@@ -0,0 +1,9 @@
+#Área do trápezio
+
+B = float(input("Digite a base maior do trapézio:\n"))
+b = float(input("Digite a base menor do trapézio:\n"))
+h = float(input("Digite a altura do trapézio:\n"))
+
+area = (((B+b)*h)/2)
+
+print("A área do trapézio é de",area,"metros quadrados.")
