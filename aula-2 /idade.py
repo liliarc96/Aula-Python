0a485669db759400f9a1752c3fb0eb449a322100
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-02 21:13:08 -0300

    Create idade.py

---
+++ aula-2 /idade.py
@@ -0,0 +1,14 @@
+#• Faça um algoritmo que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
+#• A idade desta pessoa;
+#• Quantos anos ela terá em 2025;
+
+nascimento = int(input("Digite o ano do seu nascimento:\n"))
+ano_atual = int(input("Digite o ano atual:\n"))
+
+idade = ano_atual - nascimento
+
+print("Você tem",idade,"anos.\n")
+
+idade = 2025 - nascimento
+
+print("Você terá",idade,"anos em 2025.")
