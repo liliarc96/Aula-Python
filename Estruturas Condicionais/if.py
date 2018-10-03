a6032733a643d7962c0d21926e9982061d73a7d1
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-02 21:44:02 -0300

    Update if.py

--- Estruturas Condicionais/if.py
+++ Estruturas Condicionais/if.py
@@ -3,3 +3,16 @@ if idade >= 18:
   print("Maior or igual a 18")
 else:
   print("Menor de 18")
+
+  
+  
+  
+  media = 7.0
+
+if media >= 7.0:
+  print("Aprovado")
+else:
+  if media < 4:
+    print("Reprovado")
+  else:
+    print("Final")
