308f9190ff1e685de456d45f82fa19d4bfbb89b8
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-03 20:52:57 -0300

    Create habilitacao.py

---
+++ aula-3/habilitacao.py
@@ -0,0 +1,8 @@
+#Sabendo que a idade necessária para obter habilitação é 18 anos, fazer um programa que lê a idade de uma pessoa e informa se ela pode ou não dirigir.
+
+idade = int(input("Digite a sua idade:\n"))
+
+if idade >= 18:
+	print("Você pode dirigir!")
+else:
+	print("Você não pode dirigir!")
