096ac011e5b65d19dcd746cb8d4af4e24592723a
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-03 21:03:55 -0300

    Create positivo-negativo.py

---
+++ aula-3/positivo-negativo.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia um número e determine se o mesmo é positivo ou negativo
+
+n = int(input("Digite um número:\n"))
+
+if n >= 0:
+	print("Número positivo.")
+else:
+	print("Número negativo.")
