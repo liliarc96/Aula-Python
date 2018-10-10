ed30f98ccc0901ec27af8694aae12f1b3224be99
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-10 15:56:13 -0300

    Update and rename aula-3/media.py to Exercicios/media.py

--- Exercicios/media.py
+++ Exercicios/media.py
@@ -1,4 +1,7 @@
-#Calcular a média:
+'''
+• Faça um programa que receba três notas de um aluno, calcule sua média final e diga se o mesmo
+está aprovado ou reprovado (se sua média for maior que 5, está aprovado)
+'''
 
 n1 = float(input("Digite a sua primeira nota:\n"))
 n2 = float(input("Digite a sua segunda nota:\n"))
