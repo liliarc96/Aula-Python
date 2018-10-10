f4c38d0a1a1dcfb8e1ac882a667d53cc6c126933
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-10 15:58:31 -0300

    Update and rename aula-3/futebol.py to Exercicios/futebol.py

--- Exercicios/futebol.py
+++ Exercicios/futebol.py
@@ -1,5 +1,7 @@
-#Faça um programa que lê o número de gols marcados pelo Sport e o número de gols marcados pelo Náutico.
-#Escrever o nome do time vencedor.Caso não haja vencedor, escrever EMPATE;
+'''
+•Faça um programa que lê o número de gols marcados pelo Sport e o número de gols marcados pelo Náutico.
+Escrever o nome do time vencedor.Caso não haja vencedor, escrever EMPATE;
+'''
 
 sport = int(input("Insira o número de gols marcados pelo Sport:\n"))
 nautico = int(input("Insira o número de gols marcados pelo Náutico:\n"))
