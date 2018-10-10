7cdd1b3ae1fefdba5863515ed52097bedcb91758
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-10 15:42:43 -0300

    Update and rename aula-2 /idade.py to Exercicios/idade.py

--- Exercicios/idade.py
+++ Exercicios/idade.py
@@ -1,6 +1,8 @@
-#• Faça um algoritmo que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
-#• A idade desta pessoa;
-#• Quantos anos ela terá em 2025;
+'''
+• Faça um algoritmo que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
+• A idade desta pessoa;
+• Quantos anos ela terá em 2025;
+'''
 
 nascimento = int(input("Digite o ano do seu nascimento:\n"))
 ano_atual = int(input("Digite o ano atual:\n"))
