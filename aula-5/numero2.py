0b11ebef61ca6e296a07a25593bde4f7989752c8
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-10 21:21:54 -0300

    Create numero2.py

---
+++ aula-5/numero2.py
@@ -0,0 +1,15 @@
+'''
+• Exemplo: Faça um programa que sempre recebe
+números inteiros e conta quantos números foram
+iguais a 2. Caso o usuário digite o número 0, o
+programa deverá parar de receber valores e exibir
+quantos números foram iguais a 2;
+'''
+numero = 1
+contador = 0
+
+while numero != 0:
+	numero = int(input("Digite um número:"))
+	if numero == 2:
+		contador = contador + 1
+print("O número 2 foi digitado",contador, "vezes.")
