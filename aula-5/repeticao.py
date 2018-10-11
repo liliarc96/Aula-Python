91adb7e5cab7d8ddd1481df7bd6e9108f161bec5
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-10 21:05:34 -0300

    Create repeticao.py

---
+++ aula-5/repeticao.py
@@ -0,0 +1,12 @@
+'''
+• Exemplo: Faça um programa que lê repetidamente
+vários números inteiros e os escreve na tela. O
+mesmo deverá repetir este procedimento enquanto
+números diferentes de zero forem digitados;
+'''
+
+numero = 1
+
+while (numero != 0):
+	numero = int(input("Digite um número:\n"))
+	print(numero)
