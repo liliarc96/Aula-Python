3a6676cb6d3ab142ad294147645d02ead9531783
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-17 20:38:07 -0300

    Create positivo_negativo.py

---
+++ Exercicios_2/positivo_negativo.py
@@ -0,0 +1,16 @@
+'''
+• Faça um programa que lê 10 números, calcula e
+exibe:
+• O triplo de cada número;
+• A mensagem “É positivo”, caso o número digitado seja
+positivo, ou “É negativo”, caso seja negativo;
+'''
+cont = 1
+
+while (cont <= 10):
+	numero = int(input("Digite um número:"))
+	if (numero >= 0):
+		print("É positivo!")
+	else:
+		print("É negativo!")
+	cont += 1
