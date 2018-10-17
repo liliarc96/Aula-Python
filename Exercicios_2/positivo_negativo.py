cbd6950a0f4b1b5cbecdca402be453070f78a889
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-17 20:42:20 -0300

    Update positivo_negativo.py

--- Exercicios_2/positivo_negativo.py
+++ Exercicios_2/positivo_negativo.py
@@ -14,3 +14,12 @@ while (cont <= 10):
 	else:
 		print("É negativo!")
 	cont += 1
+
+cont_2 = 0
+
+for cont_2 in range (0,10):
+	numero = int(input("Digite um número:"))
+	if (numero >= 0):
+		print("É positivo!")
+	else:
+		print("É negativo!")	
