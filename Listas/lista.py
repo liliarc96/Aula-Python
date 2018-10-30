078ddf59fcc34b03caf5d296db2fd9adc753931e
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-30 20:49:16 -0300

    Create lista.py

---
+++ Listas/lista.py
@@ -0,0 +1,10 @@
+'''
+• LISTA
+'''
+
+numeros = [10, 20, 30]
+nomes = ["José", "Maria", "Carlos"]
+dados = ["alo", 2.0, 5, nomes]
+
+idades = [int(input("Digite a primeira idade:")),int(input("Digite a segunda idade:")),int(input("Digite a terceira idade:"))]
+print(idades)
