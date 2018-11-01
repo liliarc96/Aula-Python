02a01625698116f4fd2e100e0de4ba2a19109cc4
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-10-31 21:06:59 -0300

    Update cinema.py

--- Exercicios_3/cinema.py
+++ Exercicios_3/cinema.py
@@ -27,3 +27,4 @@ for rank in telespectadores:
 	elif rank == 1:
 		regular += 1
 print("\nA quantidade de pessoas que respondeu ótimo foi de:",otimo,"\nA quantidade de pessoas que respondeu bom foi de:",bom,"\nA quantidade de pessoas que respondeu regular foi de:",regular)
+.
