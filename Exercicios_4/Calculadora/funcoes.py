36fede64ca171a2eca1deb6e3e766867e43e41d4
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-11-21 21:46:03 -0200

    Create funcoes.py

---
+++ Exercicios_4/Calculadora/funcoes.py
@@ -0,0 +1,14 @@
+'''
+
+\/\/Funções\/\/
+
+'''
+def calculadora(x,y,code):
+	if code == "+":
+		return x+y
+	elif code == "-":
+		return x-y
+	elif code == "*":
+		return x*y
+	elif code == "/":
+		return x/y
